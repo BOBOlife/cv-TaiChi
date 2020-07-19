d1658e18389c7325b7e91bd912f1c2a29d59c391
BOBOWang <bbw1996@qq.com>
2020-07-19 19:14:30 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,73 @@
+let style = document.querySelector("#style");
+let html = document.querySelector("#html");
+let str = `/* 你好，我是王波
+*  接下来我来演示一下我的前端功底
+*  首先我要准备一个div
+**/
+#div1{
+    border:1px solid red;
+    width:200px;
+    height:200px;
+}
+/* 接下来就把div变成一个八卦图
+ * 注意了，开始看我秀
+ * 首先将div变成一个圆
+ **/
+#div1{
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border:none;
+}
+/*八卦是合阴而抱阳，阴阳相容而成的
+ * 一黑一白
+ **/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/* 两个神秘的小球*/
+#div1::before{
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
+`;
+let str2 = "";
+let n = 0;
+let step = () => {
+  setTimeout(() => {
+    //若是回车，不照搬
+    //若不是回车就照搬
+    if (str[n] === "\n") {
+      str2 += "<br>";
+    } else if (str[n] === " ") {
+      str2 += "&nbsp;";
+    } else {
+      str2 += str[n];
+    }
+    html.innerHTML = str2;
+    style.innerHTML = str.substring(0, n);
+    window.scrollTo(0, 99999);
+    html.scrollTo(0, 99999);
+    if (n < str.length - 1) {
+      //若 n 不是最后一个，就继续
+      n += 1;
+      step();
+    }
+  }, 40);
+};
+step();
